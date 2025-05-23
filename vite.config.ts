84afe620ac5fa28f79426eae91f46a9b6b4aa298
TeaCoder52 <vnechaev888@gmail.com>
2025-05-23 12:59:47 +0300

    chore: add basic SEO tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd())
-
-	const PORT = Number(env['VITE_PORT']) ?? 6000
-	const ALLOWED_HOSTS = env['VITE_ALLOWED_HOSTS']
-		? env['VITE_ALLOWED_HOSTS'].split(',').map((host) => host.trim())
-		: ['localhost']
-
-	return {
-		plugins: [react(), tailwindcss()],
-		server: {
-			host: true,
-			port: PORT,
-			watch: {
-				usePolling: true,
-			},
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+	server: {
+		host: true,
+		watch: {
+			usePolling: true,
 		},
-		preview: {
-			allowedHosts: ALLOWED_HOSTS,
-		},
-	}
+	},
 })
